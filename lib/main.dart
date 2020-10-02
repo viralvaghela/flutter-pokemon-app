19f4b09883f1832c7d952ab9ae5f1d0d684f1664
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-10-02 12:37:01 +0530

    Adds Multiple Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon/model/DarkThemeProvider.dart';
+import 'package:pokemon/model/Styles.dart';
 import 'package:pokemon/screens/homepage_screen.dart';
+import 'package:provider/provider.dart';
 
-void main()=>runApp(MyApp());
-class MyApp extends StatelessWidget
-{
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = new DarkThemeProvider();
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentTheme();
+  }
+
+  void getCurrentTheme() async {
+    themeChangeProvider.darkTheme =
+        await themeChangeProvider.darkThemePreference.getTheme();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     debugShowCheckedModeBanner: false,
-      home: Homepage(),
+    return ChangeNotifierProvider(
+      create: (_) {
+        return themeChangeProvider;
+      },
+      child: Consumer<DarkThemeProvider>(
+        builder: (context, value, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: Styles.themeData(themeChangeProvider.darkTheme, context),
+            home: Homepage(),
+          );
+        },
+      ),
     );
   }
 }
